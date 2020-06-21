ff3ee37d997b5f0a968500d8974af9a10b160803
Sumit Chahal <smtchahal@gmail.com>
2020-06-21 14:23:45 +0530

    Updates

---
+++ precache-manifest.8759f3fc0945adf40b514ff9206fb7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15957360e3c403758d307c1e45beef99",
+    "url": "/gta-snap-to-jpg/index.html"
+  },
+  {
+    "revision": "8afed71ce2e0b38c47d6",
+    "url": "/gta-snap-to-jpg/static/css/main.483005c9.chunk.css"
+  },
+  {
+    "revision": "13ebc31355c412bb452c",
+    "url": "/gta-snap-to-jpg/static/js/2.35b8e28f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gta-snap-to-jpg/static/js/2.35b8e28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afed71ce2e0b38c47d6",
+    "url": "/gta-snap-to-jpg/static/js/main.62d9ac96.chunk.js"
+  },
+  {
+    "revision": "e8c4df6b79533e0a8272",
+    "url": "/gta-snap-to-jpg/static/js/runtime-main.88af2bb8.js"
+  }
+]);
